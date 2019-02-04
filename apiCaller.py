ad6877474ab8b1ec551ff90990f2b61731357d5b
rargh <eetu.t.narhi@gmail.com>
2019-02-04 15:17:57 +0100

    Create country list function

--- apiCaller.py
+++ apiCaller.py
@@ -2,13 +2,13 @@ import requests
 
 country_url = "http://api.worldbank.org/v2/country/all?format=json&per_page=304"
 country_json = requests.get(country_url).json()
-country_amount = country_json[0]['total']
-
-# Return list of countries
 
 
 def get_country_list():
-    pass
+    country_list = []
+    for country in country_json[1]:
+        country_list.append(country['name'])
+    return country_list
 
 
 def get_country_code(name):
@@ -21,3 +21,6 @@ def get_population(name, year):
 
 def get_emissions(name, year):
     pass
+
+
+print(get_country_list())
