0c87c5f7995dbfc1f56ef63af6315d2e16e15f67
rargh <eetu.t.narhi@gmail.com>
2019-02-04 15:45:51 +0100

    Create get_country_code and inner function get_country_id_dict

--- apiCaller.py
+++ apiCaller.py
@@ -3,6 +3,7 @@ import requests
 country_url = "http://api.worldbank.org/v2/country/all?format=json&per_page=350"
 country_json = requests.get(country_url).json()
 country_list = []
+country_id_dict = {}
 
 
 def get_country_list():
@@ -15,8 +16,17 @@ def get_country_list():
     return country_list
 
 
+def get_country_id_dict():
+    global country_id_dict
+    if len(country_id_dict) == 0:
+        for country in country_json[1]:
+            country_id_dict[country['name']] = country['id'].lower()
+
+    return country_id_dict
+
+
 def get_country_code(name):
-    pass
+    return get_country_id_dict()[name]
 
 
 def get_population(name, year):
