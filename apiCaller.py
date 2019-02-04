e7b5bb926b425a1e53f24369f892ed8a7cf5ee34
rargh <eetu.t.narhi@gmail.com>
2019-02-04 15:26:15 +0100

    Improved get_country_list to not re-populate on each call

--- apiCaller.py
+++ apiCaller.py
@@ -1,13 +1,17 @@
 import requests
 
-country_url = "http://api.worldbank.org/v2/country/all?format=json&per_page=304"
+country_url = "http://api.worldbank.org/v2/country/all?format=json&per_page=350"
 country_json = requests.get(country_url).json()
+country_list = []
 
 
 def get_country_list():
-    country_list = []
-    for country in country_json[1]:
-        country_list.append(country['name'])
+    global country_list
+    if len(country_list) == 0:
+        country_list = []
+        for country in country_json[1]:
+            country_list.append(country['name'])
+
     return country_list
 
 
@@ -21,6 +25,3 @@ def get_population(name, year):
 
 def get_emissions(name, year):
     pass
-
-
-print(get_country_list())
