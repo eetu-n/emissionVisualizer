984c27db7e11bde93dce03e8f118869081c0f4f9
rargh <eetu.t.narhi@gmail.com>
2019-02-06 15:00:02 +0100

    Created simple html selector that populates with country list

--- app.py
+++ app.py
@@ -1,11 +1,16 @@
-from flask import Flask
+from flask import Flask, render_template
+from apiCaller import ApiCaller
 
 app = Flask(__name__)
 
+apiCaller = ApiCaller()
 
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
+
+@app.route('/', methods=["POST", "GET"])
+def index():
+    country_list = apiCaller.get_country_list()
+
+    return render_template('index.html', countryList=country_list)
 
 
 if __name__ == '__main__':
