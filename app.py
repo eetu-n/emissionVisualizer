ff92039efee34d626f7d04788ea556fe78252402
rargh <eetu.t.narhi@gmail.com>
2019-02-06 16:55:38 +0100

    Created simple selection UI with parameters passed correctly.

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from apiCaller import ApiCaller
 
 app = Flask(__name__)
@@ -9,8 +9,20 @@ apiCaller = ApiCaller()
 @app.route('/', methods=["POST", "GET"])
 def index():
     country_list = apiCaller.get_country_list()
+    year_list = apiCaller.temp_get_year_list()
+    country = "blank"
+    year = 0000
+    data = "blank"
+    received = False
 
-    return render_template('index.html', countryList=country_list)
+    if request.method == "POST":
+        country = request.form.get('selected_country')
+        year = request.form.get('selected_year')
+        data = request.form.get('selected_data')
+        received = True
+
+    return render_template('index.html', country_list=country_list, year_list=year_list, country=country, year=year,
+                           data=data, received=received)
 
 
 if __name__ == '__main__':
