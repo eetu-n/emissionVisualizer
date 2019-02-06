4a78557e49805a72e925bfac1a30a139e0103a19
rargh <eetu.t.narhi@gmail.com>
2019-02-06 17:04:55 +0100

    Created UI for passing values of data requested

--- app.py
+++ app.py
@@ -12,17 +12,22 @@ def index():
     year_list = apiCaller.temp_get_year_list()
     country = "blank"
     year = 0000
-    data = "blank"
+    data_type = "blank"
+    data = 0000
     received = False
 
     if request.method == "POST":
         country = request.form.get('selected_country')
         year = request.form.get('selected_year')
-        data = request.form.get('selected_data')
+        data_type = request.form.get('selected_data')
+        if data_type == "emissions":
+            data = apiCaller.get_emissions(country, year)
+        else:
+            data = apiCaller.get_population(country, year)
         received = True
 
     return render_template('index.html', country_list=country_list, year_list=year_list, country=country, year=year,
-                           data=data, received=received)
+                           data_type=data_type, data=data, received=received)
 
 
 if __name__ == '__main__':
